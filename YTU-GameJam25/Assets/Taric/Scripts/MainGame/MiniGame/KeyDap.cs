39e303a9e3c857765015fce23cc7d003b5f67d97
Mehmet Tarık Albayrak <albayrakmehmettarik@gmail.com>
2025-05-31 10:26:39 +0300

    UnityMallasti

---
+++ YTU-GameJam25/Assets/Taric/Scripts/MainGame/MiniGame/KeyDap.cs
@@ -0,0 +1,37 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeyDap : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI CodeText;
+    string codeTextValue;
+    public string safeCode;
+
+    void Start()
+    {
+    
+    }
+
+    private void Update()
+    {
+        CodeText.text = codeTextValue;
+
+        if (codeTextValue == safeCode)
+        {
+            //buraya uyarýyý yok etme ve prefeb objesini yok etme kodunu girmen lazýmke//
+            SceneManager.LoadScene(0);
+        }
+
+        if (codeTextValue.Length > 4)
+        {
+            codeTextValue = "";
+        }
+    }
+
+    public void AddDigit(string digit)
+    {
+        codeTextValue += digit;
+    }
+}
